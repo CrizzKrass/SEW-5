5a6828c3a27a0a2e58b750855a1f888cc993dc07
chrisi_holz <christoph.holzinger@sz-ybbs.ac.at>
2023-03-17 11:47:09 +0100

    UnitTests in C# hinzugefügt

---
+++ UnitTestsExample/Meth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsExample
+{
+    public class Meth
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        public double Div(int x, int y)
+        {
+            return (double)x / y;
+        }
+        public int Fuck(int x)
+        {
+            int ret = 1;
+            for(int i = 1; i <= x; i++)
+            {
+                ret *= i;
+            }
+            return ret;
+        }
+        public int Mod(int x, int y)
+        {
+            return x % y;
+        }
+    }
+
+}
---
+++ UnitTestsTest/MethTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestsExample;
+
+namespace UnitTestsTest
+{
+    internal class MethTest
+    {
+        [Test]
+        public void TestAddTrue()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Add(1, 2);
+
+            Assert.AreEqual(3, currentResult);
+        }
+        [Test]
+        public void TestAddFalse()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Add(1, 2);
+
+            Assert.AreNotEqual(4, currentResult);
+        }
+        [Test]
+        public void TestSubTrue()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Sub(5, 2);
+
+            Assert.AreEqual(3, currentResult);
+        }
+        [Test]
+        public void TestSubFalse()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Sub(5, 2);
+
+            Assert.AreNotEqual(4, currentResult);
+        }
+        [Test]
+        public void TestMulTrue()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Mul(5, 2);
+
+            Assert.AreEqual(10, currentResult);
+        }
+        [Test]
+        public void TestMulFalse()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Mul(5, 2);
+
+            Assert.AreNotEqual(4, currentResult);
+        }
+        [Test]
+        public void TestDivTrue()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Div(5, 2);
+
+            Assert.AreEqual(2.5, currentResult);
+        }
+        [Test]
+        public void TestDivFalse()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Div(5, 2);
+
+            Assert.AreNotEqual(4, currentResult);
+        }
+        [Test]
+        public void TestFuckTrue()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Fuck(5);
+
+            Assert.AreEqual(120, currentResult);
+        }
+        [Test]
+        public void TestModTrue()
+        {
+            var meth = new Meth();
+            var currentResult = meth.Mod(5,3);
+
+            Assert.AreEqual(2, currentResult);
+        }
+    }
+}

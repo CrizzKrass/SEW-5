f470394b9af564a9ffa3694692ba4d95674f5f58
chrisi_holz <christoph.holzinger@sz-ybbs.ac.at>
2023-03-24 11:51:30 +0100

    Test Driven Development Example hinzugefügt

---
+++ UnitTestsExample/FizzBuzzConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsExample
+{
+    public class FizzBuzzConverter
+    {
+        private IValueConverter defaultConverter;
+        private IValueConverter[] valueConverters;
+        public FizzBuzzConverter(IValueConverter defaultConverter, params IValueConverter[] converter)
+        {
+            this.valueConverters = converter;
+            this.defaultConverter = defaultConverter;
+        }
+
+        public String Convert(int number)
+        {
+            string res = "";
+
+            foreach(var converter in this.valueConverters)
+            {
+                res += converter.Convert(number);
+            }
+
+            if (string.IsNullOrEmpty(res))
+            {
+                return this.defaultConverter.Convert(number);
+            }
+
+            return res;
+        }
+
+    }
+
+    public interface IValueConverter
+    {
+        string Convert(int number);
+    }
+
+    public class FizzConvert : IValueConverter
+    {
+        public string Convert(int number)
+        {
+
+            if (number % 3 == 0) return "Fizz";
+
+            return "";
+        }
+    }
+
+    public class BuzzConvert : IValueConverter
+    {
+        public string Convert(int number)
+        {
+
+            if (number % 5 == 0) return "Buzz";
+
+            return "";
+        }
+    }
+
+    public class NumberConvert : IValueConverter
+    {
+        public string Convert(int number)
+        {
+            return number.ToString();
+        }
+    }
+}
---
+++ UnitTestsTest/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestsExample;
+
+namespace UnitTestsTest
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(3, ExpectedResult = "Fizz")]
+        [TestCase(5, ExpectedResult = "Buzz")]
+        [TestCase(15, ExpectedResult = "FizzBuzz")]
+
+        public String GivenNumberIsConvertedCorrectly(int number)
+        {
+            var sut = new FizzBuzzConverter(new NumberConvert(), new FizzConvert(), new BuzzConvert());
+            return sut.Convert(number);
+        }
+    }
+}
